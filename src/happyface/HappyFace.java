057917c8c1a20a102bbd59502183dc93bfd7d02b
k.uno <k.uno@unokouheinoMacBook-Pro.local>
2018-01-22 15:53:35 +0900

    fix #4

--- src/happyface/HappyFace.java
+++ src/happyface/HappyFace.java
@@ -4,9 +4,9 @@ public class HappyFace {
 	public static void main(String[] args) {
 		Player first = new Tanaka();
 		Player last = new Sato();
-		
-		JadgementMan jadgementMan = new JadgementMan(first, last);
-		jadgementMan.gameStart();
-		jadgementMan.jadge();
+
+		JudgementMan judgementMan = new JudgementMan(first, last);
+		judgementMan.gameStart();
+		judgementMan.judge();
 	}
 }
