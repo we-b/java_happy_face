f4cfec33a8a91c9eb2ef816085a9dd026db39b42
k.uno <k.uno@unokouheinoMacBook-Pro.local>
2018-01-22 15:51:34 +0900

    fix #3

--- src/happyface/Player.java
+++ src/happyface/Player.java
@@ -2,13 +2,14 @@ package happyface;
 
 abstract public class Player {
 	private AI ai;
-	private int winCount = 0;	
+	private int winCount = 0;
 	private Profile profile;
-	
+
 	public Player() {
-		 profile=createProfile();
+		 ai = createAi();
+		 profile = createProfile();
 	}
-	
+
 	public Direction offense() {
 		return ai.decideAttackAction();
 	}
@@ -16,20 +17,20 @@ abstract public class Player {
 	public Direction defense() {
 		return ai.decideDefenseAction();
 	}
-	
+
 	public void notifyWin()
 	{
 		winCount += 1;
 	}
-	
+
 	public int getWinCount(){
 		return winCount;
 	}
-	
+
 	public String getName() {
 		return profile.getName();
 	}
-	
+
 	abstract protected Profile createProfile();
 	abstract protected AI createAi();
 }
