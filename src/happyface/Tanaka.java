b63d1462ceb6cb6a7e92e62dcf9692efa77abeb6
nakazawasho <syodobvsyo@gmail.com>
2018-01-31 11:27:08 +0900

    done

--- src/happyface/Tanaka.java
+++ src/happyface/Tanaka.java
@@ -6,7 +6,7 @@ public class Tanaka extends Player {
 	protected Profile createProfile() {
 		Profile profile = new Profile();
 		profile.setAge(10);
-		profile.setName("tanaka");
+		profile.setName("田中");
 		profile.setSex(Sex.MAN);
 		return profile;
 	}
--- src/happyface/TurnAI.java
+++ src/happyface/TurnAI.java
@@ -1,18 +1,44 @@
 package happyface;
 
+import java.util.Random;
+
 public class TurnAI implements AI {
-	private Direction direction = Direction.DOWN;
+	private Direction direction;
 
 	@Override
 	public Direction decideAttackAction() {
+		decideAction();
 		direction = getNextDirection(direction);
 		return direction;
 	}
 
 	@Override
 	public Direction decideDefenseAction() {
+		decideAction();
 		direction = getNextDirection(direction);
 		return direction;
+	}
+
+	public void decideAction() {
+		Random rnd = new Random();
+		int num = rnd.nextInt(4);
+		
+		switch (num) {
+		case 0:
+			this.direction = Direction.DOWN;
+			break;
+		case 1:
+			this.direction = Direction.UP;
+			break;
+		case 2:
+			this.direction = Direction.LEFT;
+			break;
+		case 3:
+			this.direction = Direction.RIGHT;
+			break;
+		default:
+			this.direction = Direction.UP;
+		}
 	}
 
 	private Direction getNextDirection(Direction preDirection) {
@@ -30,3 +56,4 @@ public class TurnAI implements AI {
 		}
 	}
 }
+
